08b638183f4327eea227d46e04efef20d133c541
Karn Kaul <karnkaul@gmail.com>
2021-10-26 23:07:53 -0700

    Add tagged_store, delegate

---
+++ include/ktl/delegate.hpp
@@ -0,0 +1,174 @@
+// KTL header-only library
+// Requirements: C++17
+
+#pragma once
+#include "move_only_function.hpp"
+#include "tagged_store.hpp"
+
+namespace ktl {
+namespace detail {
+template <typename... Args>
+using delegate_callback = move_only_function<void(Args const&...)>;
+} // namespace detail
+
+///
+/// \brief Store for observers and RAII handles
+///
+template <typename T, typename StorePolicy = tagged_store_policy>
+class observer_store;
+
+///
+/// \brief Delegate with customizable store policy
+///
+template <typename StorePolicy, typename... Args>
+class policy_delegate;
+
+///
+/// \brief Delegate: store for callbacks; provides RAII signal instances
+///
+template <typename... Args>
+using delegate = policy_delegate<tagged_store_policy, Args...>;
+
+template <typename T, typename StorePolicy>
+class observer_store : public tagged_store<T, StorePolicy> {
+  public:
+	using store_t = tagged_store<T, StorePolicy>;
+	using tag_t = typename store_t::tag_t;
+
+	///
+	/// \brief RAII handle for attaching / detaching Ts
+	///
+	class handle;
+
+	constexpr observer_store() = default;
+	constexpr observer_store(observer_store&& rhs) noexcept : observer_store() { exchg(*this, rhs); }
+	constexpr observer_store& operator=(observer_store&& rhs) noexcept { return (exchg(*this, rhs), *this); }
+	constexpr ~observer_store() noexcept { clear(); }
+
+	[[nodiscard]] constexpr tag_t attach(T t) { return this->push(std::move(t)); }
+	constexpr bool detach(tag_t tag) { return this->pop(tag); }
+	[[nodiscard]] constexpr handle make_handle() noexcept { return this; }
+
+	constexpr void clear() noexcept;
+
+  private:
+	static constexpr void exchg(observer_store& lhs, observer_store& rhs) noexcept;
+
+	constexpr void exchg(handle const* existing, handle* replace) noexcept;
+	constexpr void track(handle* signal) { m_handles.push_back(signal); }
+	constexpr void untrack(handle const* signal) { m_handles.erase(std::remove(m_handles.begin(), m_handles.end(), signal)); }
+
+	typename StorePolicy::template store_t<handle*> m_handles;
+
+	friend class handle;
+};
+
+template <typename T, typename StorePolicy>
+class observer_store<T, StorePolicy>::handle {
+  public:
+	constexpr handle() = default;
+	constexpr handle(handle&& rhs) noexcept : handle() { exchg(*this, rhs); }
+	constexpr handle& operator=(handle&& rhs) noexcept { return (exchg(*this, rhs), *this); }
+	constexpr ~handle();
+
+	constexpr bool active() const noexcept { return m_delegate != nullptr; }
+	explicit constexpr operator bool() const noexcept { return active(); }
+
+	constexpr tag_t attach(T t);
+	constexpr bool detach(tag_t tag);
+	constexpr void clear() noexcept;
+	constexpr bool operator+=(T t) { return attach(std::move(t)); }
+
+  private:
+	static constexpr void exchg(handle& lhs, handle& rhs) noexcept;
+
+	constexpr handle(observer_store* del) : m_delegate(del) { m_delegate->track(this); }
+
+	typename StorePolicy::template store_t<tag_t> m_tags;
+	observer_store* m_delegate{};
+
+	friend class observer_store;
+};
+
+template <typename StorePolicy, typename... Args>
+class policy_delegate : public observer_store<detail::delegate_callback<Args...>> {
+  public:
+	using policy_t = StorePolicy;
+	using callback = move_only_function<void(Args const&...)>;
+	using signal = typename observer_store<callback>::handle;
+
+	[[nodiscard]] signal make_signal() { return this->make_handle(); }
+
+	void operator()(Args const&... t) const { dispatch(t...); }
+	void dispatch(Args const&... t) const {
+		for (callback const& cb : *this) { cb(t...); }
+	}
+};
+
+// impl
+
+template <typename T, typename StorePolicy>
+constexpr void observer_store<T, StorePolicy>::clear() noexcept {
+	for (auto& h : m_handles) { h->m_delegate = {}; }
+	m_handles.clear();
+	store_t::clear();
+}
+
+template <typename T, typename StorePolicy>
+constexpr void observer_store<T, StorePolicy>::exchg(observer_store& lhs, observer_store& rhs) noexcept {
+	for (auto& h : lhs.m_handles) { h->m_delegate = &rhs; }
+	for (auto& h : rhs.m_handles) { h->m_delegate = &lhs; }
+	std::swap(lhs.m_handles, rhs.m_handles);
+	std::swap(static_cast<store_t&>(lhs), static_cast<store_t&>(rhs));
+}
+
+template <typename T, typename StorePolicy>
+constexpr void observer_store<T, StorePolicy>::exchg(handle const* existing, handle* replace) noexcept {
+	for (auto& s : m_handles) {
+		if (s == existing) {
+			s = replace;
+			break;
+		}
+	}
+}
+
+template <typename T, typename StorePolicy>
+constexpr observer_store<T, StorePolicy>::handle::~handle() {
+	clear();
+	if (m_delegate) { m_delegate->untrack(this); }
+}
+
+template <typename T, typename StorePolicy>
+constexpr auto observer_store<T, StorePolicy>::handle::attach(T t) -> tag_t {
+	if (active()) {
+		auto const ret = m_delegate->attach(std::move(t));
+		m_tags.push_back(ret);
+		return ret;
+	}
+	return StorePolicy::null_tag_v;
+}
+
+template <typename T, typename StorePolicy>
+constexpr bool observer_store<T, StorePolicy>::handle::detach(tag_t tag) {
+	if (active() && m_delegate->detach(tag)) {
+		m_tags.erase(std::remove(m_tags.begin(), m_tags.end(), tag), m_tags.end());
+		return true;
+	}
+	return false;
+}
+
+template <typename T, typename StorePolicy>
+constexpr void observer_store<T, StorePolicy>::handle::clear() noexcept {
+	if (active() && !m_tags.empty()) {
+		for (auto const tag : m_tags) { m_delegate->detach(tag); }
+	}
+}
+
+template <typename T, typename StorePolicy>
+constexpr void observer_store<T, StorePolicy>::handle::exchg(handle& lhs, handle& rhs) noexcept {
+	if (lhs.m_delegate) { lhs.m_delegate->exchg(&lhs, &rhs); }
+	if (rhs.m_delegate) { rhs.m_delegate->exchg(&rhs, &lhs); }
+	std::swap(lhs.m_delegate, rhs.m_delegate);
+	std::swap(lhs.m_tags, rhs.m_tags);
+}
+} // namespace ktl
---
+++ include/ktl/tagged_store.hpp
@@ -0,0 +1,127 @@
+// KTL header-only library
+// Requirements: C++17
+
+#pragma once
+#include <algorithm>
+#include <cstdint>
+#include <iterator>
+#include <vector>
+
+namespace ktl {
+struct tagged_store_policy {
+	template <typename T>
+	using store_t = std::vector<T>;
+	using tag_t = std::uint64_t;
+	static constexpr tag_t null_tag_v = tag_t{};
+};
+
+///
+/// \brief Storage for individually tagged Ts; supports popping T via associated tag and bidirectional iteration
+///
+template <typename T, typename Policy = tagged_store_policy>
+class tagged_store {
+	template <bool Const>
+	class iter_t;
+
+  public:
+	using policy_t = Policy;
+	using tag_t = typename Policy::tag_t;
+	using iterator = iter_t<false>;
+	using const_iterator = iter_t<true>;
+	using reverse_iterator = std::reverse_iterator<iterator>;
+	using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+	static constexpr tag_t null_tag_v = Policy::null_tag_v;
+
+	[[nodiscard]] constexpr tag_t push(T t);
+	constexpr bool pop(tag_t tag);
+
+	constexpr std::size_t size() const noexcept { return m_store.size(); }
+	constexpr void clear() noexcept { m_store.clear(); }
+	constexpr bool empty() const noexcept { return m_store.empty(); }
+
+	constexpr iterator begin() noexcept { return m_store.begin(); }
+	constexpr iterator end() noexcept { return m_store.end(); }
+	constexpr reverse_iterator rbegin() noexcept { return reverse_iterator(iterator(m_store.end())); }
+	constexpr reverse_iterator rend() noexcept { return reverse_iterator(iterator(m_store.begin())); }
+	constexpr const_iterator cbegin() noexcept { return m_store.cbegin(); }
+	constexpr const_iterator cend() noexcept { return m_store.cend(); }
+	constexpr const_iterator begin() const noexcept { return m_store.begin(); }
+	constexpr const_iterator end() const noexcept { return m_store.end(); }
+	constexpr const_reverse_iterator crbegin() noexcept { return const_reverse_iterator(const_iterator(m_store.end())); }
+	constexpr const_reverse_iterator crend() noexcept { return const_reverse_iterator(const_iterator(m_store.begin())); }
+	constexpr const_reverse_iterator rbegin() const noexcept { return const_reverse_iterator(const_iterator(m_store.end())); }
+	constexpr const_reverse_iterator rend() const noexcept { return const_reverse_iterator(const_iterator(m_store.begin())); }
+
+  private:
+	struct entry_t {
+		T t;
+		tag_t tag = null_tag_v;
+	};
+	using store_t = typename Policy::template store_t<entry_t>;
+
+	store_t m_store;
+	tag_t m_next = null_tag_v;
+};
+
+template <typename T, typename Policy>
+template <bool Const>
+class tagged_store<T, Policy>::iter_t {
+	using iterator_t = std::conditional_t<Const, typename store_t::const_iterator, typename store_t::iterator>;
+
+  public:
+	using iterator_category = std::bidirectional_iterator_tag;
+	using type = T;
+	using value_type = T;
+	using difference_type = typename iterator_t::difference_type;
+	using pointer = std::conditional_t<Const, T const*, T*>;
+	using reference = std::conditional_t<Const, T const&, T&>;
+
+	constexpr iter_t() = default;
+
+	constexpr reference operator*() const noexcept { return m_it->t; }
+	constexpr pointer operator->() const noexcept { return &m_it->t; }
+
+	constexpr iter_t& operator++() noexcept { return (++m_it, *this); }
+	constexpr iter_t& operator--() noexcept { return (--m_it, *this); }
+	constexpr iter_t operator++(int) noexcept {
+		auto ret = *this;
+		++m_it;
+		return ret;
+	}
+	constexpr iter_t operator--(int) noexcept {
+		auto ret = *this;
+		--m_it;
+		return ret;
+	}
+
+	friend constexpr bool operator==(iter_t lhs, iter_t rhs) noexcept { return lhs.m_it == rhs.m_it; }
+	friend constexpr bool operator!=(iter_t lhs, iter_t rhs) noexcept { return !(lhs == rhs); }
+
+  private:
+	constexpr iter_t(iterator_t it) noexcept : m_it(it) {}
+
+	iterator_t m_it;
+
+	friend class tagged_store;
+};
+
+// impl
+
+template <typename T, typename Policy>
+constexpr auto tagged_store<T, Policy>::push(T t) -> tag_t {
+	tag_t ret = ++m_next;
+	m_store.push_back({std::move(t), ret});
+	return ret;
+}
+
+template <typename T, typename Policy>
+constexpr bool tagged_store<T, Policy>::pop(tag_t tag) {
+	auto it = std::remove_if(m_store.begin(), m_store.end(), [tag](entry_t const& e) { return e.tag == tag; });
+	if (it != m_store.end()) {
+		m_store.erase(it, m_store.end());
+		return true;
+	}
+	return false;
+}
+} // namespace ktl
