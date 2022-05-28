15eb7989ac3bd29a191ce0a67e234a424a2d998a
Karn Kaul <karnkaul@gmail.com>
2022-05-28 09:46:45 -0700

    v1.3: Add and use kversion

---
+++ include/ktl/kversion.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <compare>
+
+namespace ktl {
+///
+/// \brief Semantic version
+///
+struct kversion {
+	int major{};
+	int minor{};
+	int patch{};
+
+	auto operator<=>(kversion const&) const = default;
+
+	template <typename OstreamT>
+	friend constexpr OstreamT& operator<<(OstreamT& out, kversion const& v) {
+		return out << 'v' << v.major << '.' << v.minor << '.' << v.patch;
+	}
+
+	template <typename IstreamT>
+	friend constexpr IstreamT& operator<<(IstreamT& in, kversion& out) {
+		char discard;
+		return in >> discard >> out.major >> discard >> out.minor >> discard >> out.patch;
+	}
+};
+} // namespace ktl
